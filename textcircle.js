c87a4b3af95e2c36d4f0f59f80e447faa9a11b2d
Juan Aguilar <juanmanaguilar@gmail.com>
2016-03-07 20:24:27 +0100

    Documento colaborativo basico con share.js Code Mirror

---
+++ textcircle.js
@@ -0,0 +1,28 @@
+this.Documents = new Mongo.Collection("documents");
+
+if (Meteor.isClient) {
+    Template.editor.helpers({
+        docid:function(){
+            console.log("editor helper");
+            console.log(Documents.findOne());
+ //           return Documents.findOne()._id;
+            var doc = Documents.findOne();
+            if (doc){
+                return doc._id;
+            }
+            else {
+                return undefined;
+            }
+        }
+    });
+ 
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+      if (!Documents.findOne()){
+          Documents.insert({title: "My new document"});
+      }
+  });
+}
