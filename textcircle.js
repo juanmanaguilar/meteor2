555ca77e373f7f24e9cc9f37d18e58a2b9184644
Juan Aguilar <juanmanaguilar@gmail.com>
2016-03-08 09:36:48 +0100

    Se añade navigation bar fixed top usando twbs:bootstrap

--- textcircle.js
+++ textcircle.js
@@ -2,24 +2,12 @@ this.Documents = new Mongo.Collection("documents");
 
 if (Meteor.isClient) {
     
-    Meteor.setInterval(function(){
-        // update the current_date variable every 1000 ms
-        // la metemos en sesion para que use la propiedad reactive y recargue
-        // cada vez que cambie la variable
-        Session.set("current_date", new Date());
-    }, 1000);
     
-    Template.date_display.helpers({
-        current_date: function(){
-            return Session.get("current_date");
-        }
-    });
     
     Template.editor.helpers({
         docid:function(){
             console.log("editor helper");
             console.log(Documents.findOne());
- //           return Documents.findOne()._id;
             var doc = Documents.findOne();
             if (doc){
                 return doc._id;
